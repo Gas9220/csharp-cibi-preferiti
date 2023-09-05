9e57f5fa089956339389c3bcf19bd3a956aaa028
Gaspare <Gas9220@users.noreply.github.com>
2023-09-05 15:17:35 +0200

    Lenght of foods array

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,5 @@
 ﻿// Inizializzo l'array di cibi
 string[] foods = { "Banana", "Pasta", "Coconut", "Pizza", "Tofu", "Lentils", "Potatoes" };
+
+// Lenght of the array
+Console.WriteLine($"There are {foods.Length} foods on the array");
