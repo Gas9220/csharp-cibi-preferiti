f8184878143dbcb986df5b962fd91fcdc2f6c5e9
Gaspare <Gas9220@users.noreply.github.com>
2023-09-05 15:45:38 +0200

    Fix last food and bonus completed

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Inizializzo l'array di cibi
-string[] foods = { "Banana", "Pasta", "Coconut", "Pizza", "Tofu", "Lentils", "Potatoes" };
+string[] foods = { "Banana", "Pasta", "Coconut", "Pizza", "Tofu", "Lentils", "Tomatoes", "Orange" };
 
 // Lenght of the array
 Console.WriteLine($"There are {foods.Length} foods on the array");
@@ -14,4 +14,18 @@ for (int i = 0; i < foods.Length; i++)
 Console.WriteLine($"My top food is {foods[0]}");
 
 // Last food
-Console.WriteLine($"My last top food is {foods[6]}");
+Console.WriteLine($"My last top food is {foods[foods.GetUpperBound(0)]}");
+
+// If foods array lenght is odd
+if ( foods.Length % 2 != 0)
+{
+    string middleElement = foods[foods.Length / 2];
+    Console.Write("Foods lenght is odd - ");
+    Console.WriteLine($"The middle element is {middleElement}");
+} else
+{
+    string middleElement1 = foods[foods.Length / 2];
+    string middleElement2 = foods[foods.Length / 2 - 1];
+    Console.Write("Foods lenght is odd - ");
+    Console.WriteLine($"The two middle elements are {middleElement1}, {middleElement2}");
+}
